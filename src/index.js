be39559174fa431e556b3e07ef60ea1d18f03152
pogorelova-v <gorelikovavn@gmail.com>
2022-06-27 15:41:46 +0300

    feat: variables with basic numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function toReadable (number) {
-  
+
+    let result = '';
+   
+    let zero = 'zero';  //0 
+    let one = 'one';    //1
+    let two = 'two';    //2 
+    let three = 'three';    //3
+    let four = 'four';  //4 
+    let five = 'five';  //5
+    let six = 'six';    //6
+    let seven = 'seven';    //7
+    let eight = 'eight';    //8 
+    let nine = 'nine';  //9
+    let ten = 'ten';    //10
+    let eleven = 'eleven';  //11
+    let twelve = 'twelve';  //12    
+    let thirteen = 'thirteen';  //13
+    let fourteen = 'fourteen';  //14
+    let fifteen = 'fifteen';  //15
+    let sixteen = 'sixteen';  //16
+    let seventeen = 'seventeen';  //17
+    let eighteen = 'eighteen';  //18
+    let nineteen = 'nineteen';  //19
+    let twenty = 'twenty';  //20
+    let thirty = 'thirty';  //30
+    let forty = 'forty';  //40
+    let fifty = 'fifty';  //50
+    let sixty = 'sixty';  //60
+    let seventy = 'seventy';  //70
+    let eighty = 'eighty';  //80
+    let ninety = 'ninety';  //90
+    let hundred = 'hundred';  //100 - one hundred
+
+     
+    return result;
 }
