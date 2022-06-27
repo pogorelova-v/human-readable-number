bae728c1222b662700699866ae937f031d2521c8
pogorelova-v <gorelikovavn@gmail.com>
2022-06-27 17:00:59 +0300

    feat: new function

--- src/index.js
+++ src/index.js
@@ -32,6 +32,41 @@ module.exports = function toReadable (number) {
     let ninety = 'ninety';  //90
     let hundred = 'hundred';  //100 - one hundred
 
-     
+    const units = [zero, one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen];
+
+    const tens = [zero, one, twenty, thirty, forty, fifty, sixty, seventy, eighty, ninety];
+
+    let numStr = String(number);
+
+    if ( number >= 0 && number <= 19) {
+        result = units[number]
+    } else if ( numStr.length === 2 && number >= 20){
+          if (number === 20 || number === 30 || number === 40 || number === 50 || number === 60 || number === 70 || number === 80 || number === 90){
+            result = tens[number]
+          } else if ( numStr[0] === 2 && number > 20) {
+            result = twenty + units[1];
+          } else if ( numStr[0] === 3 && number > 30) {
+            result = thirty + units[1];
+          } else if ( numStr[0] === 4 && number > 40) {
+            result = forty + units[1];
+          } else if ( numStr[0] === 5 && number > 50) {
+            result = fifty + units[1];
+          } else if ( numStr[0] === 6 && number > 60) {
+            result = sixty + units[1];
+          } else if ( numStr[0] === 7 && number > 70) {
+            result = seventy + units[1];
+          } else if ( numStr[0] === 8 && number > 80) {
+            result = eighty + units[1];
+          } else if ( numStr[0] === 9 && number > 90) {
+            result = ninety + units[1];
+          }
+    } else if (numStr.length === 3 && number >= 100 && number % 100 === 0) {
+        result = units[numStr[0]] + ' ' +hundred;
+    } else if (numStr.length === 3 && number > 100 && number % 100 !== 0 && numStr[2] !==0){
+        result = units[numStr[0]] + ' ' + hundred + ' ' + tens[numStr[1]] + ' ' + units[numStr[2]]
+    } else if (numStr.length === 3 && number > 100 && number % 100 !== 0 && numStr[2] === 0){
+        result = units[numStr[0]] + ' ' + hundred + ' ' + tens[numStr[1]]
+    }
+
     return result;
 }
